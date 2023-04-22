c4d55c25fc7261b946a0e01b6051390f1b58ab48
brohithkr <brohithkr@gmail.com>
2023-04-22 21:31:16 +0530

    code

--- hi.py
+++ hi.py
@@ -1,23 +1,5 @@
+import numpy as np
 
-def subsequences(s,res):
-    if len(s) == 0:
-        return res
-    charf = s[0]
-    print(charf,s[1:])
-    res2 = subsequences(s[1:], res)
-    # res2arr = res2.split(",")
-    print(res2)
-    for i in res2:
-        res+=[i]
-        # res+=","
-        res+= [charf + i]
-        # res+=","
-    return res
-    
-if __name__ == "__main__":
-    s = "hello"
+arr = np.array([2,3,4,5])
 
-    res = []
-
-    res2 = subsequences(s, res)
-    # print(res2)
+print(arr)
