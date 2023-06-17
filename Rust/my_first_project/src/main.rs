075b2b0eab6e5db3c3d3c9a006b8b8315e62db86
bannnaiah <rohith7644489@gmail.com>
2023-06-18 00:40:56 +0530

    code

--- Rust/my_first_project/src/main.rs
+++ Rust/my_first_project/src/main.rs
@@ -1,7 +1,13 @@
+use std::io;
+
 fn main() {
     let mut fname = String::new();
     let mut lname = String::new();
-    std::io::stdin().read_line(&mut fname).expect("failed to read line");
-    std::io::stdin().read_line(&mut lname).expect("failed to read line");
+    io::stdin()
+    .read_line(&mut fname)
+    .expect("failed to read line");
+    io::stdin()
+    .read_line(&mut lname)
+    .expect("failed to read line");
     println!("Hello {} {}",fname.trim_end(),lname.trim_end());
 }
