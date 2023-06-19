19cef91942a3c7360d479b350fc79edae968eb45
bannnaiah <rohith7644489@gmail.com>
2023-06-20 00:32:45 +0530

    vofr

--- Rust/my_first_project/src/main.rs
+++ Rust/my_first_project/src/main.rs
@@ -1,13 +1,9 @@
-
-
+// use std::io;
 
 fn main() {
-    let x: u32 = "24".parse().expect("Not a number!");
-    {
-        let x = x*2;
-        println!("{x}");
-    }
-    println!("{x}");
-
-    
+    let a = {
+        let x = 4;
+        x + 2
+    };
+    println!("{a}");
 }
