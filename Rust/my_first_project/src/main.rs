680e143d4766895b0c9a9e13c5c96af623e7ceaa
bannnaiah <rohith7644489@gmail.com>
2023-06-21 12:40:16 +0530

    code

--- Rust/my_first_project/src/main.rs
+++ Rust/my_first_project/src/main.rs
@@ -1,9 +1,44 @@
-// use std::io;
+use std::io;
+
+fn fibonacci(n: i32) {
+    let mut prev = 1;
+    let mut prevprev = 1;
+    let mut k = 1;
+    while k<n {
+        if k==1{
+            println!("1");
+        }
+        else if k==2{
+            println!("1");
+        }
+        else {
+            let num: usize = prev + prevprev;
+            println!("{}",num);
+            prevprev = prev;
+            prev = num;
+        }
+        k+=1;
+    }
+    
+}
 
 fn main() {
-    let a = {
-        let x = 4;
-        x + 2
-    };
-    println!("{a}");
+    let mut i = String::new();
+
+    io::stdin()
+    .read_line(&mut i)
+    .expect("Error");
+
+    let i: i32 = i
+    .trim()
+    .parse()
+    .expect("Not an integer.");
+
+    fibonacci(i);
+
+    // let a: [i32; 5 ] = [12,24,36,54,23];
+
+ 
+
+
 }
