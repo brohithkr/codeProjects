c98305b14874181227d932ce78c69c4035e85f21
brohithkr <brohithkr@gmail.com>
2023-07-21 16:57:38 +0530

    code

--- Rust/vec_operator/src/main.rs
+++ Rust/vec_operator/src/main.rs
@@ -1,10 +1,17 @@
-fn ask_and_push(v: &mut Vec<i32>) {
+fn ask_and_push(v: &mut Vec<i32>) -> bool {
     let stdin = std::io::stdin();
     println!("Please give a number to push.");
     let mut num = String::new();
     stdin.read_line(&mut num).expect("Unable to take input.");
-    let num: i32 = num.trim().parse().unwrap();
+    let num: i32 = match num.trim().parse() {
+        Ok(n) => n,
+        Err(_e) => {
+            println!("Please provide a valid number.");
+            return false;
+        }
+    };
     v.push(num);
+    return true
 }
 
 fn pop_vec(v: &mut Vec<i32>) {
@@ -20,7 +27,13 @@ fn get_a_num(v: &Vec<i32>) -> bool {
     println!("Enter a position: ");
     let mut pos = String::new();
     stdin.read_line(&mut pos).expect("Unable to take input.");
-    let pos: usize = pos.trim().parse().unwrap();
+    let pos: usize = match pos.trim().parse() {
+        Ok(n) => n,
+        Err(_e) => {
+            println!("Please provide a number.");
+            return false;
+        } 
+    };
     let num = (v).get(pos);
     match num {
         Some(n) => {
@@ -41,9 +54,17 @@ fn main() {
         println!("Choose among the following options:\n1. Push a number\n2. Pop a number\n3. Get a number\n4. Print the vector\n5. Exit");
         let mut opt = String::new();
         stdin.read_line(&mut opt).expect("Unable to take input.");
-        let opt: i32 = opt.trim().parse().unwrap();
+        let opt: i32 = match opt.trim().parse() {
+            Ok(n) => n,
+            Err(_e) => {
+                println!("Please provide a valid number.");
+                continue;
+            }
+        };
         if opt == 1 {
-            ask_and_push(&mut v)
+            while !ask_and_push(&mut v) {
+                println!("Please try again.")
+            }
         } else if opt == 2 {
             pop_vec(&mut v);
         } else if opt == 3 {
