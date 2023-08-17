ed28cc80bc61999a5b66f9cfaf91a699e5c90f8e
brohithkr <brohithkr@gmail.com>
2023-08-17 09:47:20 +0530

    code

---
+++ TS/hello.ts
@@ -0,0 +1,4 @@
+function hello(name: string){
+    return "hello" + name; 
+}
+console.log(hello("rohith"))
--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-import socket
+import matplotlib.pyplot as plt
+import numpy as np
 
+x = np.arange(40)
+y = (np.exp(x))
 
-def send_file(file_path, ip_address):
-    with open(file_path, 'rb') as f:
-        data = f.read()
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip_address, 1234))
-    s.sendall(data)
-    s.close()
-
-send_file("test.txt", "192.168.16.1")
+plt.plot(x,y)
+# plt.show()
+plt.savefig("hello.svg")
+# plt.close()
+# plt.savefig("hello2.svg")
